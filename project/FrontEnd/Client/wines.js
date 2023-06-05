55f7395eaf035f3b2ffb5c470e45a4349d741fe4
Dindosss <andindaworking@gmail.com>
2023-06-05 20:55:57 +0200

    Update wines.js

--- project/FrontEnd/Client/wines.js
+++ project/FrontEnd/Client/wines.js
@@ -6,12 +6,145 @@ window.onload = function(){
     switchOnLoader();
 
     xhttpObject.onreadystatechange = function() {
-        if(this.readyState == 4 && this.status == 200)placeWineElements(this.responseText);
+        if(this.readyState == 4 && this.status == 200){
+            switchOffLoader();
+            document.querySelector(".website-container").innerHTML = "";
+            placeWineElements(this.responseText);
+        }
     };
 
     xhttpObject.open("GET", "../../Backend/Api/Api.php?type=GET_WINE&lastcount=0");
     xhttpObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttpObject.send(body);
+    xhttpObject.send();
+}
+
+const loadDefault = function(){
+    document.getElementById("searchbar").value = "";
+    const xhttpObject = new XMLHttpRequest();
+    switchOnLoader();
+
+    xhttpObject.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            switchOffLoader();
+            document.querySelector(".website-container").innerHTML = "";
+            placeWineElements(this.responseText);
+        }
+    };
+
+    xhttpObject.open("GET", "../../Backend/Api/Api.php?type=GET_WINE&lastcount=0");
+    xhttpObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttpObject.send();
+}
+
+const searchFor = function() {
+    const searchbarval = document.getElementById("searchbar").value;
+    searchval = searchbarval;
+    switchOnLoader();
+
+    const xhttpObject = new XMLHttpRequest();
+    xhttpObject.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            switchOffLoader();
+            document.querySelector(".website-container").innerHTML = "";
+            placeWineElements(this.responseText);
+        }
+    };
+
+    xhttpObject.open("GET", "../../Backend/Api/Api.php?type=SEARCH_WINE&lastcount=0&name=" + searchbarval);
+    xhttpObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttpObject.send();
+}
+
+const switchOnLoader = function(){
+    const websiteContainer = document.querySelector(".website-container");
+    websiteContainer.innerHTML += '<div class="spinner-container">' +
+                                    '<div class="spinner-grow text-success" role="status">' +
+                                        '<span class="sr-only">Loading...</span>' +
+                                    '</div>' +
+                                '</div>';
+}
+
+const switchOffLoader = function(){
+    document.querySelector(".spinner-container").remove();
+}
+
+const placeWineElements = function(res){
+    const jsonRes = JSON.parse(res);
+    const websiteContainer = document.querySelector(".website-container");
+
+    for(let i = 0; i < jsonRes.data.length; ++i){
+        websiteContainer.innerHTML += '<div class="card card-item rounded-2" style="width: 18rem;">'+
+                                        '<div class="img-container">'+
+                                            '<img class="card-img-top" src="'+ jsonRes.data[i].wine_imageURL +'" alt="Card image cap">'+
+                                        '</div>'+
+                                        '<div class="card-body">'+
+                                        '<h5 class="card-title">'+ jsonRes.data[i].wine_name +'</h5>' + 
+                                        '<p class="card-text">'+ jsonRes.data[i].winery_name +'</p>'+
+                                        '</div>'+
+                                        '<ul class="list-group list-group-flush">'+
+                                        '<li class="list-group-item"> <i class="fa-solid fa-circle-notch"></i> &nbsp;'+ jsonRes.data[i].varietal +'</li>'+
+                                        '<li class="list-group-item"> <i class="fa-solid fa-palette"></i> &nbsp;'+ jsonRes.data[i].colour  +' </li>'+
+                                        '<li class="list-group-item"> <i class="fa-solid fa-cubes-stacked"></i> &nbsp;'+ jsonRes.data[i].carbonation +' •  &nbsp;'+ jsonRes.data[i].sweetness +' </li>'+
+                                        '<li class="list-group-item"><i class="fa-regular fa-calendar"></i> &nbsp; year bottled: '+ jsonRes.data[i].year_bottled +'</li>'+
+                                        '</ul>'+
+                                    '</div>';
+    }
+    lastServedID = jsonRes.lastcount;
+    // On Click
+    const divs = document.querySelectorAll('.card');
+    // Add click event listener to each div
+    divs.forEach(div => {
+        div.addEventListener('click', () => {
+            // Get the title element within the div
+            const titleElement = div.querySelector('.card-title');
+            // Get the text of the title element
+            const titleText = titleElement.textContent;
+            localStorage.setItem('winery_name', titleText);
+            window.location.href = "wine-details.php";
+        });
+    });
+}
+
+const loadMoreData = function(){
+    const xhttpObject = new XMLHttpRequest();
+    switchOnLoader();
+    xhttpObject.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            switchOffLoader();
+            placeWineElements(this.responseText);
+        }
+    };
+
+    xhttpObject.open("GET", "../../Backend/Api/Api.php?" + "type=SEARCH_WINE&lastcount=" + lastServedID + "&name=" + searchval);
+    xhttpObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttpObject.send();
+}
+
+const openWine = function(){
+    const xhttpObject = new XMLHttpRequest();
+    switchOnLoader();
+
+    xhttpObject.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200){
+            switchOffLoader();
+            window.location.href = "wine-details.php";
+        }
+    };
+
+    xhttpObject.open("GET", "../../Backend/Api/Api.php?type=OPEN_WINE&id=" + wineID);
+    xhttpObject.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttpObject.send();
+}
+
+const checkValue = function(){
+    console.log("here")
+    const searchbarval = document.getElementById("searchbar").value;
+    if(searchbarval === "")document.querySelector(".cancel-search-btn").hidden = true;
+    else document.querySelector(".cancel-search-btn").hidden = false;
+}
+
+const filterBy = function(){
+
 }
 
 $(document).ready(function(){
@@ -20,39 +153,49 @@ $(document).ready(function(){
         console.log("Insideee");
         var typeWine = $(this).html();
         console.log(typeWine);
-        var body = {
-            type : 'GET_WINES',
-            location : flocation
-        }
+        var body = KnowFilter(typeWine);
+        console.log(JSON.stringify(body));
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function()
         {
             if(this.readyState== 4 && this.status == 200)
             {
                 document.querySelector(".website-container").innerHTML = "";
+                placeWineElements(this.responseText);
             }
         }
+
+        xhr.open("POST","../../Backend/Api/Api.php")
+        xhr.send(JSON.stringify(body));
     })
+
 })
 
-const placeWineElements = function(res){
-    const jsonRes = JSON.parse(res);
-    const websiteContainer = document.querySelector(".website-container");
-
-    for(let i = 0; i < jsonRes.data.length; ++i){
-        websiteContainer.innerHTML += '<div class="card card-item rounded-2" style="width: 18rem;">' +
-                                '<div class="img-container">' +
-                                '<img class="card-img-top" src="'+ jsonRes.data[i].wine_imageURL +'" alt="Card image cap">' +
-                                '</div>' +
-                                '<div class="card-body">' +
-                                '<h5 class="card-title">'+ jsonRes.data[i].wine_name +'</h5>' +
-                                '<p class="card-text">'+ jsonRes.data[i].description +'</p>' +
-                                '</div>' +
-                                '<ul class="list-group list-group-flush">' +
-                                '<li class="list-group-item">' + jsonRes.data[i].varietal +'</li>' +
-                                '<li class="list-group-item"><i class="fa fa-home "></i>' + jsonRes.data[i].winery_name
-                                '</ul>' +
-                            '</div>';
+function KnowFilter(str)
+{
+    var Pbody;
+    if(str == "Red" || str== "White" )
+    {
+        Pbody = {
+            type : "GET_WINE",
+            filters : {colour : str}
+        }
     }
-    lastServedId = jsonRes.data[jsonRes.data.length - 1];
+    else if(str == "Bone Dry" || "Sparkling")
+    {
+        Pbody = {
+            type : "GET_WINE",
+            filters : {carbonation : str}
+        }
+    }
+    else if(str == "Bordeaux" || str== "Champagne")
+    {
+        Pbody = {
+            type : "GET_WINE",
+            filters : {varietal : str}
+        }
+    }
+
+    return Pbody;
+
 }
